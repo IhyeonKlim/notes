6a7d6ae84c58c76ecc70b283cb1e02a38adcc261
IheyonKlim <104817519+IheyonKlim@users.noreply.github.com>
2022-05-03 17:49:34 +0900

    Server enviroment
    node.js
    node-ts
    java for cote.

---
+++ nomadCodes/typeScript/src/index.ts
@@ -0,0 +1,15 @@
+let a = 0;
+let b = 0;
+
+function compare(a: number,b: number){
+    if(a > b)
+        return ">"
+    else if(a<b)
+        return "<"
+    else if(a==b)
+        return "=="
+}
+
+
+
+

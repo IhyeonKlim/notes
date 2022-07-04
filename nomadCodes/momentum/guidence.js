3e065f9fd28f605ffe75ae32285330bb3bd1ec46
IheyonKlim <104817519+IheyonKlim@users.noreply.github.com>
2022-07-04 17:58:14 +0900

    momentum
    prepare for study.

---
+++ nomadCodes/momentum/guidence.js
@@ -0,0 +1,56 @@
+guidence
+0. what is js?
+
+1. after html -> js, css, head.
+    alert("hi")
+2.crome based. type style. try it.
+    int, float, string. etc
+3.console.log
+    +, *, /, % + variable, num and string.
+
+4. const, let
+    const = can not be changed. constans should never be changed.
+    const : default, let : somethims.
+
+5. snake, camel.
+    veryLongVariableName
+    very_long_variable_name
+
+6. boolean. true, false.
+    "true" is string. 
+    const amIHansome = true
+
+7. null, undefined
+    emptiness. not defiined.
+
+8. list just array of things.
+    const mon, tue. daysOfWeek.
+    const nonsense =[1,"2",three, false,null,undefined]
+    daysOfWeek[0] count from 0.
+    daysOfWeek.push()
+    daysOfWeek.pop()
+    daysOfWeek.reverse()
+    daysOfWeek.splice()
+
+9. Objects Map, JSON
+    const player_name =""
+    const player_hansome = true
+    const player_age = 27
+    player.name -> wants to do.
+
+    const player = {name : "", hansome : "", age :""}
+    player.name = "" 
+    player.lastName ="" ok
+    const inside of const is variable;
+    update available.
+
+10 function
+    console.log("hello" + "eunji")
+    function functionName (name){
+        console.log("hello" + name)
+    }
+    functionName("eunji")
+
+
+
+

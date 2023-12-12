455d92afe47a8fd29b3b03d4d96254df9f73db33
IHyeon Klim <104817519+IhyeonKlim@users.noreply.github.com>
2023-12-12 15:18:03 +0900

    Update prac.ts

--- prac.ts
+++ prac.ts
@@ -1,2 +1,18 @@
 let a = "hello";
 console.log("hello");
+4ca35-2bb14
+eaecb-d688f
+76c2d-e6b2b
+7c089-9f57f
+b30f0-28b21
+3af1e-b9107
+d2e75-dd7dc
+479dc-598dd
+8fb21-f6b75
+8fb7d-3c90a
+a4b0c-3eddb
+cc3f9-af050
+9d893-a0c09
+b5f88-2e300
+62768-0cc6c
+91fd9-7f4cb
